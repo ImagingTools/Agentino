2a269d5a93c9b955cf3b5d6be4be1fe946428169
victorscherbina <victor.scherbina@gmgcolor.com>
2026-08-06 15:14:07 +0600

    AGENTINODIR_BUILD corrected

--- conanfile.py
+++ conanfile.py
@@ -152,6 +152,7 @@ class AgentinoConan(ConanFile):
         tc.variables["USE_FIND_PACKAGE"] = True
         tc.variables["CMAKE_CXX_STANDARD"] = 17
         tc.variables['BUILDDIR'] = self.build_path.as_posix()
+        tc.variables['AGENTINODIR_BUILD'] = self.build_path.as_posix()
         tc.variables['PYTHONEXE'] = Path(sys.executable).as_posix()
         # fvisibility hidden
         tc.variables['CMAKE_POLICY_DEFAULT_CMP0063'] = 'NEW'
